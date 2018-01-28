636ab130ef01ef0126fa0c849c8b3b7dbf4db484
hezixuan <hezixuan@hzyushi.cn>
2018-01-28 19:26:59 +0800

    Initial Commit

---
+++ CPlusProject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CPlusProject
+//
+//  Created by hezixuan on 2018/1/28.
+//  Copyright © 2018年 hezixuan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
